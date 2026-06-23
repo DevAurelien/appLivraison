28fc8cd6239a6a4b711eab6455233bd154c81852
Dev_Aurelien <lhuillier.aurelien47@gmail.com>
2026-06-23 17:36:06 +0000

    Codespace

--- backAppLivraison/index.js
+++ backAppLivraison/index.js
@@ -7,7 +7,11 @@ import routeUsers from "./routes/route.users.js"
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}));
+app.use(cookieParser())
 app.use(express.json())
 app.use("/", routeDeliveries );
 app.use("/", routeUsers);
