9b383162941dd94815379db592807c14181421fd
Dev_Aurelien <lhuillier.aurelien47@gmail.com>
2026-06-30 21:52:09 +0200

    V2 liv

--- Front/src/Page/pageLivraisons/Livraisons.jsx
+++ Front/src/Page/pageLivraisons/Livraisons.jsx
@@ -2,7 +2,7 @@ import CardDocuments from "../pageProfil/CardDocuments"
 export default function Livraisons() {
   return (
     <div className="flex w-full h-full bg-(--bg-main) text-6xl flex flex-col items-center p-4">
-        <CardDocuments>{<h1>Nono tu es la meilleure !! Une veritable amie ! Sincèrement, merci d'etre là, merci d'être toi</h1>}</CardDocuments>
+        <CardDocuments>Site en travaux</CardDocuments>
         
     </div>
   )
